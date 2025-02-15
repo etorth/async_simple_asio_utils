5ecd8d3b46bb9f6163d6dd12fd13f381b1419a3e
etorth <anhonghe@gmail.com>
2025-02-14 18:51:51 -0800

    f

--- corof/main.cpp
+++ corof/main.cpp
@@ -3,17 +3,8 @@
 #include <cstdint>
 #include "corof.hpp"
 
-corof::eval_poller<> k()
-{
-    std::cout << "begin wait" << std::endl;
-    hres_timer t;
-    co_await corof::async_wait(1000);
-    std::cout << "end   wait: " << t.diff_secf() << std::endl;
-}
-
 corof::eval_poller<int> f()
 {
-    co_await k();
     co_return 12;
 }
 
@@ -21,11 +12,6 @@ corof::eval_poller<std::string> g()
 {
     co_return std::string("7");
 }
-
-// corof::eval_poller<const char *> g()
-// {
-//     co_return "7";
-// }
 
 corof::eval_poller<int> r()
 {
