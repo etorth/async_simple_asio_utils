70149318f5f60d06f32e2fa709dad71f862377bd
etorth <anhonghe@gmail.com>
2025-02-17 20:52:10 +0800

    f

--- actor_with_coroutine/new/actor.cpp
+++ actor_with_coroutine/new/actor.cpp
@@ -31,13 +31,13 @@ MsgOptCont Actor::send(int toAddress, int respID, std::string message, bool wait
 
     co_return Message
     {
-        .content = "No actor found",
+        .content = "NO ACTOR FOUND",
     };
 }
 
 void Actor::receive(const Message& msg)
 {
-    printMessage("Actor %llu received message from Actor %d with content: %s, seqID: %d, respID: %s\n", address, msg.from, msg.content.c_str(), msg.seqID, (msg.respID != 0 ? std::to_string(msg.respID).c_str() : "null"));
+    printMessage("Actor %d receive: %s, from %d, seqID: %d, respID: %d\n", getAddress(), msg.content.c_str(), msg.from, msg.seqID, msg.respID);
     updateLastMsg(msg);
 
     if(msg.respID > 0){
@@ -78,7 +78,7 @@ MsgOptCont Actor::onFreeMessage(const Message &msg)
 
     if(msg.seqID > 0){
         if(msg.content == "?"){
-            auto askMsg = co_await send(msg.from, 0, "ask");
+            auto askMsg = co_await send(msg.from, 0, "ASK BACK");
             co_return Message
             {
                 .content = std::string("Reply ") + askMsg.value().content,
@@ -111,12 +111,19 @@ void Actor::initCall()
     MsgOptContAwaitable(initCallCoroutine().handle).await_suspend(std::noop_coroutine());
 }
 
+int Rand()
+{
+    static std::mutex lock;
+    const std::lock_guard<std::mutex> lockGuard(lock);
+    return std::rand();
+}
+
 MsgOptCont Actor::initCallCoroutine()
 {
     for(int recvAddr: {getAddress() - 1, getAddress() + 1}){
-        std::string message("Hello from Actor!");
+        std::string message(Rand() % 2 ? "HELLO FROM ACTOR" : "?");
 
-        if (std::rand() % 2 == 0) {
+        if (Rand() % 2 == 0) {
             auto replyOpt = co_await send(recvAddr, 0, message, true);
             auto reply = replyOpt.value();
             printMessage("Actor %llu send: %s, seqID %d to %d, received reply from %d: %s\n", getAddress(), message.c_str(), reply.respID, recvAddr, reply.from, reply.content.c_str());
